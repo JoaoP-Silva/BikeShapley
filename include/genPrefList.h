ea17fe5ab8074ed107c1cadf67c1b72458bf39cf
Joao Pedro <joopedrof.silva@gmail.com>
2022-05-17 10:24:30 -0300

    Moved to folders

---
+++ include/genPrefList.h
@@ -0,0 +1,162 @@
+#ifndef GENPREFLIST_H
+#define GENPREFLIST_H
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <ctype.h>
+#include <queue>
+#include <algorithm>
+#include "DataTypes.h"
+
+/*Funcao: genPrefListRecursive(...){}
+*	Descricao: Funcao recursiva para gerar a lista de preferencias de uma bicicleta qualquer(lista e alocada na variavel prefList).
+*	Entrada:	char** matrix - Ponteiro para matriz de caracteres.
+*				int maxRow/ int maxCollum - Inteiro com as dimensoes da matriz, num max de linhas e colunas.
+*				coord point - Variavel do tipo coord, par (linha, coluna) da coordenada do passo recursivo que esta sendo feito.
+*				std::set<coord>& visited - Referência de set de coordenadas que salva todas as coordenadas que já foram visitadas.
+*				std::queue<coordAt>& q - Referência de fila de coordenadas a serem analisadas, com uma variavel inteira relacionada ao
+*				passo que a coordenada foi encontrada.
+*				std::vector<people>* prefList: Lista de duplas(Id pessoa, distancia encontrada) para salvar a preferencia da bicicleta.
+*				unsigned int numPeople: Inteiro positivo com o numero de pessoas que tem no problema.
+* 
+*	Retorna: void
+*/
+void genPrefListRecursive(char** matrix, int maxRow, int maxCollum, coord point, std::set<coord>& visited, std::queue<coordAt>& q, std::vector<people>* prefList, unsigned int numPeople) {
+	int level = 0;
+	//Caso a fila ja tinha sido iniciada, o level é atualizado
+	if (q.size() > 0) {
+		level = q.front().second + 1;
+		q.pop();
+	}
+	std::set<coord>::iterator it;
+	int x = point.first; int y = point.second;
+	char charAtPoint = matrix[x][y];
+	if (charAtPoint >= 'a' && charAtPoint <= 'j') {
+		int tmp = charAtPoint - '0' - 1 - '0';
+		//Caso ja tenha sido adicionado algum item ao vetor resultante, insere o proximo item de forma ordenada
+		if (!prefList->empty()) {
+			std::vector<people>::iterator prefIt = prefList->end();
+			for (int i = prefList->size() - 1; i >= 0; i--) {
+				int var = prefList->at(i).second;
+				if (prefList->at(i).second == level && prefList->at(i).first < tmp  || level > prefList->at(i).second) {
+					prefList->insert(prefIt, std::pair<unsigned int, unsigned int>(tmp, level));
+					break;
+				}
+				prefIt--;
+				if (prefIt == prefList->begin()) {
+					prefList->insert(prefIt, std::pair<unsigned int, unsigned int>(tmp, level));
+				}
+			}
+		}
+		else {
+			prefList->push_back(std::pair<unsigned int, unsigned int>(tmp, level));
+		}
+		if (prefList->size() == numPeople) {
+			return;
+		}
+	}
+	//Testa se pode aplicar BFS em cada uma das direcoes possiveis
+	if (y > 0 && (matrix[x][y - 1] != '-')) {
+		coord newCoord(x, y - 1);
+		it = visited.find(newCoord);
+		if (it == visited.end()) {
+			visited.insert(newCoord);
+			coordAt auxToNew(newCoord, level);
+			q.push(auxToNew);
+		}
+	}
+	if (y < maxCollum - 1 && (matrix[x][y + 1] != '-')) {
+		coord newCoord(x, y + 1);
+		it = visited.find(newCoord);
+		if (it == visited.end()) {
+			visited.insert(newCoord);
+			coordAt auxToNew(newCoord, level);
+			q.push(auxToNew);
+		}
+	}
+	if (x > 0 && (matrix[x - 1][y] != '-')) {
+		coord newCoord(x - 1, y);
+		it = visited.find(newCoord);
+		if (it == visited.end()) {
+			visited.insert(newCoord);
+			coordAt auxToNew(newCoord, level);
+			q.push(auxToNew);
+		}
+	}
+	if (x < maxRow -1 && (matrix[x + 1][y] != '-')) {
+		coord newCoord(x + 1, y);
+		it = visited.find(newCoord);
+		if (it == visited.end()) {
+			visited.insert(newCoord);
+			coordAt auxToNew(newCoord, level);
+			q.push(auxToNew);
+		}
+	}
+	coord next = q.front().first;
+	genPrefListRecursive(matrix, maxRow, maxCollum, next, visited, q, prefList, numPeople);
+}
+
+/*Funcao: genPrefList(char** matrix, int maxRow, int maxCollum, coord init, unsigned int numPeople){}
+*	Descricao: Funcao a ser chamada no main para gerar a lista de preferencias de uma bicicleta qualquer.
+*	Entrada:	char** matrix - Ponteiro para matriz de caracteres.
+*				int maxRow/ int maxCollum - Inteiro com as dimensoes da matriz, num max de linhas e colunas.
+*				coord init - Variavel do tipo coord, par (linha, coluna) da coordenada da bicicleta que sera gerada a lista.
+*				unsigned int numPeople: Inteiro positivo com o numero de pessoas que tem no problema
+*	
+*	Retorna: Um vetor do tipo std::vector<people> ordenado, com pares (Id pessoa, distancia bicicleta).
+*/
+std::vector<people>* genPrefList_Bicycle(char** matrix, int maxRow, int maxCollum, coord init, unsigned int numPeople){
+	std::vector<people>* prefList = new std::vector<people>;
+	std::set<coord> visited;
+	visited.insert(init);
+	std::queue<coordAt> q;
+	unsigned int level = 0;
+	genPrefListRecursive(matrix, maxRow, maxCollum, init, visited , q, prefList, numPeople);
+	return(prefList);
+}
+
+
+
+/*Funcao: genPrefList_User(std::vector<int>& initPref){}
+*	Descricao:	Funcao a ser chamada no main para gerar a lista de preferencias de um usuario qualquer.
+*	Entrada:	std::vector<int>& initPref - Referencia para um vetor de preferencias no modelo da entrada,
+*				com numeros inteiros indicando o quanto a pessoa prefere cada bicicleta. A posicao do valor na entrada
+*				indica o Id da bicicleta que a preferencia esta numerada.
+* 
+* Retorna:		void.
+*/
+void genPrefList_User(std::vector<int>& initPref) {
+	std::vector<std::pair<int, int>> pairVector;
+	for (int i = 0; i < initPref.size(); i++) {
+		pairVector.push_back(std::make_pair(initPref[i], i));
+	}
+	std::sort(pairVector.begin(), pairVector.end(), std::greater<std::pair<int, int>>());
+	std::vector<int> subv, result;
+	if (!pairVector.empty()) {
+		subv.push_back(pairVector[0].second);
+	}
+	for (int i = 1; i < initPref.size(); i++) {
+		if (pairVector[i - 1].first == pairVector[i].first) {
+			subv.push_back(pairVector[i].second);
+		}
+		else {
+			std::sort(subv.begin(), subv.end());
+			for (int i = 0; i < subv.size(); i++) {
+				result.push_back(subv[i]);
+			}
+			subv.clear();
+			subv.push_back(pairVector[i].second);
+		}
+	}
+	if (!subv.empty()) {
+		std::sort(subv.begin(), subv.end());
+		for (int i = 0; i < subv.size(); i++) {
+			result.push_back(subv[i]);
+		}
+	}
+	initPref = result;
+}
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,102 @@
+#include "genPrefList.h"
+#include "Bicycle.h"
+#include "User.h"
+#include <fstream>
+
+int main(int argc, char* argv[]) {
+	int numVis, rows, collums;
+	std::ifstream inFile;
+	inFile.open(argv[1]);
+
+	//Lendo o numero de pessoas, linhas e colunas do mapa
+	inFile >> numVis; inFile >> rows;  inFile >> collums;
+	
+	//Alocando a matriz que representa o mapa
+	char** matrix = new char* [rows];
+	for (int i = 0; i < rows; i++) {
+		matrix[i] = new char[collums];
+	}
+
+	//Alocando vetor de usuarios e bicicletas com Id setado
+	std::vector<Bicycle> bikes;
+	std::vector<User> users;
+	for (int i = 0; i < numVis; i++) {
+		Bicycle btmp(i);
+		User utmp(i);
+		bikes.push_back(btmp);
+		users.push_back(utmp);
+	}
+
+	//Lendo o mapa do arquivo de entrada
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < collums; j++) {
+			inFile >> matrix[i][j];
+			char charAtPoint = matrix[i][j];
+			if (isdigit(charAtPoint)){
+				int buff = charAtPoint - '0';
+				//Salva coordenada bicicleta
+				coord tmp(i, j);
+				bikes[buff].setLoc(tmp);
+			}
+		}
+	}
+	
+	//Adicionando a preferencia das pessoas e gerando a preferencia das bicicletas
+	for (int i = 0; i < numVis; i++) {
+		std::vector<int> pref;
+		for (int j = 0; j < numVis; j++) {
+			int tmp;
+			inFile >> tmp;
+			pref.push_back(tmp);
+		}
+		genPrefList_User(pref);
+		for (int j = 0; j < numVis; j++) {
+			users[i].addToPrefList(pref[j]);
+		}
+		coord tmp = bikes[i].getLoc();
+		std::vector<people>* buff = genPrefList_Bicycle(matrix, rows, collums, tmp, numVis);
+		bikes[i].genPrefList(buff);
+		if (!bikes[i].genRankList()) {
+			std::cout << "Erro na geracao da lista de rankings \n";
+		}
+	}
+
+	//Lista de pessoas para realizar as propostas
+	std::vector<User> proposeList;
+	for (int i = 0; i < numVis; i++) {
+		proposeList.push_back(users[i]);
+	}
+
+	/*Algoritmo de Gale-Shapley*/
+	while (!proposeList.empty()) {
+		int user = proposeList[0].getId();
+		int propose = users[user].getItemFromPref();
+		int response = bikes[propose].tryMatch(user);
+		//Se a pessoa ja propos para todas as bicicletas, ela e retirada da lista de propostas
+		if (propose == -1) {
+			proposeList.erase(proposeList.begin());
+		}
+		//Se a bicicleta proposta nao tinha par, aceita a proposta
+		else if (response == -2) {
+			users[user].setMatch(bikes[propose].getId());
+			proposeList.erase(proposeList.begin());
+		}
+		//Se a bicicleta aceitou a proposta mas tinha um par antes, atualiza o vetor de propostas
+		else if (response > -1) {
+			proposeList.erase(proposeList.begin());
+			User rejected = users[response];
+			users[response].setMatch(-1);
+			proposeList.push_back(rejected);
+			users[user].setMatch(bikes[propose].getId());
+		}
+	}
+
+	//Imprime os resultados
+	for (int i = 0; i < numVis; i++) {
+		char letter = users[i].getId() + '0' + 1 + '0';
+		int match = users[i].getMatch();
+		std::cout << letter << " " << match << "\n";
+	}
+	
+	return 0;
+}
